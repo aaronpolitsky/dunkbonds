bbc2ba616598e17c3f8e7aa95dbf215f93f0e1cf
Aaron Politsky <aaron.politsky@gmail.com>
2012-09-20 07:42:09 -0700

    initial.  goals table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwoODunkbonds::Application.config.secret_token = 'a2a34be14d38a74e80a7b2a2415361e55ebc90edcd52aac632989ce2efeac10aea130aa449eb40f9316715372d414158862a97ef79e29083ccb2dba216473144'
