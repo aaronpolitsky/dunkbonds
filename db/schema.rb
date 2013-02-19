48848bfd445a6a84001af4d40c256247978a21b3
Aaron Politsky <aaron.politsky@gmail.com>
2013-02-18 17:58:51 -0800

    hm.  may have needed to remove all two_o migrations first and re migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,82 +11,63 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218192042) do
+ActiveRecord::Schema.define(:version => 20130219010705) do
 
   create_table "accounts", :force => true do |t|
-    t.integer  "goal_id"
-    t.boolean  "is_treasury",                               :default => false
-    t.boolean  "is_escrow",                                 :default => false
-    t.decimal  "balance",     :precision => 8, :scale => 2, :default => 0.0
     t.integer  "user_id"
+    t.decimal  "initial_balance", :precision => 8, :scale => 2
+    t.decimal  "balance",         :precision => 8, :scale => 2
+    t.integer  "updated_by"
+    t.boolean  "is_escrow",                                     :default => false
+    t.boolean  "is_treasury",                                   :default => false
+    t.boolean  "is_soft_deleted",                               :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "accounts", ["user_id", "goal_id"], :name => "user_goal", :unique => true
 
   create_table "bonds", :force => true do |t|
-    t.integer  "creditor_id"
-    t.integer  "debtor_id"
-    t.integer  "qty",         :default => 0
+    t.integer  "account_id"
+    t.decimal  "maturity_value", :precision => 8, :scale => 2
+    t.datetime "maturity_date"
+    t.datetime "coupon_period"
+    t.decimal  "price",          :precision => 8, :scale => 2
+    t.integer  "updated_by"
+    t.boolean  "is_deleted",                                   :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  add_index "bonds", ["creditor_id", "debtor_id"], :name => "creditor_debtor", :unique => true
-
-  create_table "cancellations", :force => true do |t|
-    t.integer  "line_item_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "carts", :force => true do |t|
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "goal_id"
   end
 
   create_table "goals", :force => true do |t|
-    t.string   "title"
+    t.integer  "goalsetter_id"
+    t.string   "type_of"
     t.text     "description"
     t.datetime "starts_at"
     t.datetime "ends_at"
-    t.string   "period"
-    t.integer  "goalsetter_id"
+    t.boolean  "is_completed",          :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "blog_url"
+    t.string   "title"
     t.string   "blog_service_provider"
   end
 
   add_index "goals", ["goalsetter_id"], :name => "index_goals_on_goalsetter_id"
 
-  create_table "line_items", :force => true do |t|
-    t.string   "status",                                        :default => "new"
-    t.string   "type_of"
-    t.decimal  "max_bid_min_ask", :precision => 8, :scale => 2
-    t.integer  "account_id"
-    t.integer  "cart_id"
-    t.integer  "order_id"
-    t.integer  "qty"
-    t.integer  "parent_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "orders", :force => true do |t|
-    t.integer  "user_id"
+    t.integer  "account_id"
+    t.string   "type_of"
+    t.string   "status"
+    t.decimal  "max_bid",    :precision => 8, :scale => 2
+    t.decimal  "min_ask",    :precision => 8, :scale => 2
+    t.integer  "updated_by"
+    t.boolean  "is_deleted",                               :default => false
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "payments", :force => true do |t|
-    t.integer  "payee_id"
-    t.integer  "recipient_id"
-    t.decimal  "amount",       :precision => 8, :scale => 2, :default => 0.0
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer  "goal_id"
+    t.integer  "bond_id"
+    t.decimal  "price",      :precision => 8, :scale => 2
+    t.integer  "match_id"
   end
 
   create_table "posts", :force => true do |t|
@@ -96,23 +77,19 @@ ActiveRecord::Schema.define(:version => 20130218192042) do
     t.string   "url"
     t.datetime "published_at"
     t.string   "guid"
-    t.boolean  "is_visible",   :default => true
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "posts", ["guid"], :name => "index_posts_on_guid"
-
-  create_table "trades", :force => true do |t|
-    t.integer  "bid_id"
-    t.integer  "ask_id"
-    t.integer  "qty"
-    t.decimal  "price",      :precision => 8, :scale => 2
+  create_table "receipts", :force => true do |t|
+    t.integer  "payee_id"
+    t.integer  "payer_id"
+    t.integer  "goal_id"
+    t.boolean  "is_executed",                               :default => false
+    t.decimal  "amount",      :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "trades", ["bid_id", "ask_id"], :name => "bid_ask"
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "",    :null => false
@@ -125,14 +102,37 @@ ActiveRecord::Schema.define(:version => 20130218192042) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
+    t.boolean  "is_admin"
     t.boolean  "is_guest",               :default => false
+    t.boolean  "gets_email",             :default => true
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
