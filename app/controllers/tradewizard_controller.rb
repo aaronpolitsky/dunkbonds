93bf05848b1a454fc3f8233fe61c0acfd978dd0a
Aaron Politsky <aaron.politsky@gmail.com>
2013-02-21 22:44:42 -0800

    some catches for math errors in wizard.  tidying of goal nav banner

--- app/controllers/tradewizard_controller.rb
+++ app/controllers/tradewizard_controller.rb
@@ -10,11 +10,13 @@ class TradewizardController < ApplicationController
   	@trade_survey = TradeSurvey.new(params[:trade_survey])
 
     unless @trade_survey.valid?
-      flash[:error] = "Please answer each question.  <br>Yes/No<br>0-100 %<br> donation >= 0 $".html_safe
+      flash[:error] = "Please answer each question.  <br>Yes/No<br>0-99%<br> donation >= 0 $<p>".html_safe
+      if @trade_survey.errors[:likelihood_pct]
+        flash[:error] = flash[:error] + "Also, that combo of % and donation would require a very large quantity.  Things get weird at the extremes.  Try lowering the % or the donation."
+      end
       redirect_to new_goal_account_tradewizard_path(@goal, @account)
     else  
       liparams = @trade_survey.line_item_params(@goal.bond_face_value)
-      flash[:notice] = "Based on those questions, I've filled in the form on the left with what you want to add to your cart."
 
       @cart = current_or_guest_user.cart
       @line_item = @account.line_items.new(liparams)
@@ -29,7 +31,6 @@ class TradewizardController < ApplicationController
             @bond_ask.qty = liparams[:qty]
             @bond_ask.max_bid_min_ask = liparams[:max_bid_min_ask]
             
-            debugger
             if @bond_ask.save
               @cart.line_items << @bond_ask
               format.html {redirect_to @cart, :notice => "Based on your answers, you want to buy a swap and sell a bond.  I've added these to your cart." }
@@ -71,8 +72,9 @@ class TradeSurvey
   attr_accessor :is_optimistic, :likelihood_pct, :donation
 
   validates :is_optimistic, :presence => true, :inclusion => ["true", "false"]
-  validates :likelihood_pct, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}
+  validates :likelihood_pct, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 99}
   validates :donation, :presence => true, :numericality => {:greater_than_or_equal_to => 0, :less_than_or_equal_to => 100}
+  validate :qty_would_be_too_high
 
   def initialize(hsh = {})
     hsh.each do |key, value|
@@ -102,9 +104,18 @@ class TradeSurvey
       liparams[:max_bid_min_ask] = face * (self.likelihood_pct.to_d/100.0)
       liparams[:qty] = (self.donation.to_d / (1 - self.likelihood_pct.to_d/100.0)).round
     end
-    
 
     liparams
+  end
+
+  def qty_would_be_too_high
+    if self.is_optimistic == "false"
+      if (self.donation.to_d / (1 - self.likelihood_pct.to_d/100.0)).round > 100
+        self.errors.add(:likelihood_pct, "That would require a very large quantity.  Things get weird at the extremes.  Try lowering the %")
+        return false
+      end
+    end
+    return true
   end
 
 end
