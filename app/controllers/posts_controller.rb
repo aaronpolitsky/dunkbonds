9c3abb5907c22d2b5a5d05e0c3e2d39c92cb52b6
Aaron Politsky <aaron.politsky@gmail.com>
2013-03-07 23:38:14 -0800

    added post title to titlebar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
   # GET /posts/1.xml
   def show
     @post = @goal.posts.find(params[:id])
+    @title = @post.title
 
     respond_to do |format|
       format.html # show.html.erb
