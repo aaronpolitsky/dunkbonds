6d82440dc240dc53be5e109790e02c19d49f6496
Aaron Politsky <aaron.politsky@gmail.com>
2020-04-28 00:24:43 -0500

    made it to 5.2.0

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '5.0.0'
+gem 'rails', '5.2.0'
 gem 'devise'
 gem 'will_paginate'
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-my_date_formats = { :default => '%m/%d/%Y' } 
-Time::DATE_FORMATS.merge!(my_date_formats) 
-Date::DATE_FORMATS.merge!(my_date_formats)
