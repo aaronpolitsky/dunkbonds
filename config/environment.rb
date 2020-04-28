17c1c4c85c1cb9c0c92680295873f9521c8982f3
Aaron Politsky <aaron.politsky@gmail.com>
2020-04-28 00:24:54 -0500

    forgot to add

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+my_date_formats = { :default => '%m/%d/%Y' } 
+Time::DATE_FORMATS.merge!(my_date_formats) 
+Date::DATE_FORMATS.merge!(my_date_formats)
